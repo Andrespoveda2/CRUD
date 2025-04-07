86c34495d904029128ac3baac47461d47daf70c3
Andres <andrespoveda2077@gmail.com>
2025-04-07 11:51:04 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/primer/crud/PrimerCrud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primer.crud;
+
+/**
+ *
+ * @author SENA
+ */
+public class PrimerCrud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
